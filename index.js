5621922d87f49f66e7ccb958633e5b8384c4ec30
Jace Ju <jaceju@gmail.com>
2015-05-20 12:43:52 +0800

    Fix bug of options assignment

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ var elixir = require('laravel-elixir');
 var _ = require('underscore');
 var hologram = require('gulp-hologram');
 var fs = require('fs');
+var taskName = 'style-guide';
 
 elixir.extend('styleGuide', function (options) {
 
-    options = _.extend({
-        config: 'hologram.yml'
+    options = options || {};
+
+    var config = 'hologram.yml';
+
+    if (options.config) {
+        config = options.config;
+        delete options.config;
+    }
+
+    var opts = _.extend({
+        bundler: fs.existsSync('.bundle')
     }, options);
 
-    gulp.task('style-guide', function (cb) {
-        return gulp.src(options.config)
-            .pipe(hologram({
-                bundler: fs.existsSync('.bundle')
-            }));
+    gulp.task(taskName, function () {
+        return gulp.src(config)
+            .pipe(hologram(opts));
     });
 
-    return this.queueTask('style-guide');
+    return this.queueTask(taskName);
 });
